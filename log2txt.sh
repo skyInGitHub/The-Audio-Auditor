84c3665db335a9bffa1fd3edf958efcc872cb352
skyInGitHub <skymyuanti@gmail.com>
2019-04-30 16:41:45 +1000

    log2txt

---
+++ log2txt.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Convert xxx.log to xxx.csv
+##
+## Step1: xxx.log to xxx.txt ---> reduce some rebundent info
+## Step2: select strings from xxx.txt to .csv
+## Step2: formalize the xxx.csv with features
+
+
+# Get input strings from cmd
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <path/to/log> <path/to/new_txt>"
+  echo "e.g: $0 data/train/shd2_in.log data/train/train_in_2.txt"
+  exit 1
+fi
+
+
+log=$1
+txt=$2
+#log="shd1_out.log"
+
+# log2txt
+#tmp="tmp.txt"
+sed -e 'h;s/.*utterance //' $log > $txt
+sed -i '1d' $txt
+for ((i=1;i<=3;i++)); do
+  sed -i '$d' $txt
+done
+#sed -i 's/ sil//' $tmp
+
+#sed -i :a -e '$d;N;2,3ba' -e 'P;D' -i $tmp
+
+#sed -i '${lines}s/ /,/1' < input.txt
